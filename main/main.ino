ebbd5ba8978a4233e19f00335a81da3406450f9f
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-30 17:22:52 +0800

    - upload files

---
+++ main/main.ino
@@ -0,0 +1,16 @@
+#include <IRremote.hpp>
+#define IR_RECEIVE_PIN 7
+
+void setup()
+{
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
+      IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
+      IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
+      IrReceiver.resume(); // Enable receiving of the next value
+  }
+}
